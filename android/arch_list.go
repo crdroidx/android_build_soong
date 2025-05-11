35a3f63b8330c0cb0eb768c50e66ba4fe354cefc
igor121 <chernokalsky89@gmail.com>
2025-05-11 18:48:09 +0300

    Add exynos-m4 support
    * m4 is not a55 due to lack of instructions, neither is it a53 due to some extra instructions from a55
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- android/arch_list.go
+++ android/arch_list.go
@@ -100,6 +100,7 @@ var cpuVariants = map[ArchType][]string{
 		"kryo385",
 		"exynos-m1",
 		"exynos-m2",
+		"exynos-m4",
 	},
 	Arm64: {
 		"cortex-a510",
@@ -115,6 +116,7 @@ var cpuVariants = map[ArchType][]string{
 		"kryo785",
 		"exynos-m1",
 		"exynos-m2",
+		"exynos-m4",
 		"oryon",
 	},
 	X86:    {},
--- cc/config/arm64_device.go
+++ cc/config/arm64_device.go
@@ -98,6 +98,9 @@ var (
 		"exynos-m2": []string{
 			"-mcpu=exynos-m2",
 		},
+		"exynos-m4": []string{
+			"-mcpu=exynos-m4",
+		},
 	}
 )
 
@@ -132,6 +135,7 @@ func init() {
 	pctx.StaticVariable("Arm64Kryo385Cflags", strings.Join(arm64CpuVariantCflags["kryo385"], " "))
 	pctx.StaticVariable("Arm64ExynosM1Cflags", strings.Join(arm64CpuVariantCflags["exynos-m1"], " "))
 	pctx.StaticVariable("Arm64ExynosM2Cflags", strings.Join(arm64CpuVariantCflags["exynos-m2"], " "))
+	pctx.StaticVariable("Arm64ExynosM4Cflags", strings.Join(arm64CpuVariantCflags["exynos-m4"], " "))
 
 	pctx.StaticVariable("Arm64FixCortexA53Ldflags", "-Wl,--fix-cortex-a53-843419")
 }
@@ -149,6 +153,7 @@ var (
 		"kryo385":    "${config.Arm64Kryo385Cflags}",
 		"exynos-m1":  "${config.Arm64ExynosM1Cflags}",
 		"exynos-m2":  "${config.Arm64ExynosM2Cflags}",
+		"exynos-m4":   "${config.Arm64ExynosM4Cflags}",
 	}
 
 	arm64CpuVariantLdflags = map[string]string{
@@ -158,6 +163,8 @@ var (
 		"kryo":       "${config.Arm64FixCortexA53Ldflags}",
 		"exynos-m1":  "${config.Arm64FixCortexA53Ldflags}",
 		"exynos-m2":  "${config.Arm64FixCortexA53Ldflags}",
+		"exynos-m4":  "${config.Arm64FixCortexA53Ldflags}",
+
 	}
 )
 
